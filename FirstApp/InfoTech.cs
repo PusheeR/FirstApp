b69823d960574d0beefe081177b5691a733371cb
PusheeR <balta.omerfaruk@hotmail.com>
2023-01-10 21:14:31 +0300

    ...

---
+++ FirstApp/InfoTech.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public class InfoTech
+    {
+        public static void yazdir() 
+        {
+            Console.WriteLine("Selam arkadaşlar nasılsınız");
+        
+        }
+
+
+
+    }
+}
---
+++ FirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using FirstApp;
+
+{
+    string[] meyveler = { "Elma", "Karpuz", "Vişne", "Kivi" };
+	for (int i = 0; i < meyveler.Length; i++)
+		Console.WriteLine($"{i+1} Ürün Adı : " + meyveler[i]);
+
+	Console.WriteLine("---------------");
+
+	foreach (var item in meyveler)
+		Console.WriteLine("Ürün Adı : " + item);
+
+	Console.WriteLine("---------------");
+
+	List<string> listMeyveler = new List<string>();
+	listMeyveler.Add("Vişne");
+	listMeyveler.Add("Elma");
+	listMeyveler.Add("Kivi");
+	listMeyveler.Add("Karpuz");
+
+	foreach (var item in listMeyveler)
+		Console.WriteLine("List Meyve Ürün Adı : " + item);
+
+
+	Console.WriteLine("--------------");
+
+
+
+}
+
+
+
+
+
